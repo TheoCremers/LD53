b532faeb7ac42d93adf9070c6e57f040031b8e2a
Ruud <ruud88@gmail.com>
2023-04-30 00:12:27 +0200

    step movement

--- Assets/Scripts/Dungeon.cs
+++ Assets/Scripts/Dungeon.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
+using DG.Tweening;
 
 public class Dungeon : MonoBehaviour
 {
@@ -50,14 +53,42 @@ public class Dungeon : MonoBehaviour
 
     private void MovementStep()
     {
+        var nextRoomPos = DetermineNextRoom();
+        var nextRoom = Floor.Rooms[nextRoomPos.x, nextRoomPos.y];
+        Debug.Log($"Moving to {nextRoomPos}");
+
+        TweenMimicGuy(MimicGuy.transform, PositionHelper.GridToWorldPosition(nextRoomPos));
+    }
+
+    private void TweenMimicGuy(Transform mimicGuy, Vector3 destination, float tweenTime = 1f)
+    {
+        mimicGuy.DOMove(destination, tweenTime);
+    }
+
+    private Vector2Int DetermineNextRoom()
+    {
         // Check which adjacent room the character will move to
         var currentGridPosition = MimicGuy.GridPosition;
                 
-        Debug.Log("topleft: " + Floor.IsPassagePossible(currentGridPosition, Orientation.TopLeft));
-        Debug.Log("topright: " + Floor.IsPassagePossible(currentGridPosition, Orientation.TopRight));
-        Debug.Log("botleft: " + Floor.IsPassagePossible(currentGridPosition, Orientation.DownLeft));
-        Debug.Log("botright: " + Floor.IsPassagePossible(currentGridPosition, Orientation.DownRight));
+        var possibleOptions = new List<Orientation>();
+        if (Floor.IsPassagePossible(currentGridPosition, Orientation.TopLeft)) { possibleOptions.Add(Orientation.TopLeft); }
+        if (Floor.IsPassagePossible(currentGridPosition, Orientation.TopRight)) { possibleOptions.Add(Orientation.TopRight); }
+        if (Floor.IsPassagePossible(currentGridPosition, Orientation.DownLeft)) { possibleOptions.Add(Orientation.DownLeft); }
+        if (Floor.IsPassagePossible(currentGridPosition, Orientation.DownRight)) { possibleOptions.Add(Orientation.DownRight); }
 
+        // If there are no options, do nothing (player has to intervene. if he can't, game over)
+        // TODO implement game over
+        if (!possibleOptions.Any())
+        {
+            throw new UnityException("Game over chief");
+        }
+
+        // Check if the current facing direction is available. If yes, follow through
+        // TODO this logic.
+
+        // Else pick a new random direction
+        var choice = possibleOptions[Random.Range(0, possibleOptions.Count-1)];
+        return new Vector2Int(currentGridPosition.x, currentGridPosition.y) + PositionHelper.ToVector(choice);
     }
 
 
