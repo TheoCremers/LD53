30affd20c9635eae4edbe4e4381bc4b1c4f3c316
Ruud <ruud88@gmail.com>
2023-04-29 20:02:20 +0200

    doorways

--- Assets/Scripts/DungeonRoom.cs
+++ Assets/Scripts/DungeonRoom.cs
@@ -2,18 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class DungeonRoom : MonoBehaviour
 {
     public SpriteRenderer SpriteRenderer;
-    // Start is called before the first frame update
-    void Start()
+
+    public bool DoorTopLeft;
+
+    public bool DoorTopRight;
+
+    public bool DoorBottomRight;
+
+    public bool DoorBottomLeft;
+
+    private SpriteRenderer _doorTopLeft;
+    private SpriteRenderer _doorTopRight;
+    private SpriteRenderer _doorBottomRight;
+    private SpriteRenderer _doorBottomLeft;
+
+    public Sprite DoorOpenSprite;
+
+    public Sprite DoorClosedSprite;
+
+
+    void Awake()
     {
-        
+        _doorTopLeft = this.transform.Find("DoorTopLeft").GetComponent<SpriteRenderer>();
+        _doorTopRight = this.transform.Find("DoorTopRight").GetComponent<SpriteRenderer>();
+        _doorBottomRight = this.transform.Find("DoorBottomRight").GetComponent<SpriteRenderer>();
+        _doorBottomLeft = this.transform.Find("DoorBottomLeft").GetComponent<SpriteRenderer>();
+        UpdateDoorVisibility();
     }
 
-    // Update is called once per frame
+    public void UpdateDoorVisibility()
+    {
+
+        _doorTopLeft.sprite = DoorTopLeft ? DoorOpenSprite : DoorClosedSprite;
+        _doorTopRight.sprite = DoorTopRight ? DoorOpenSprite : DoorClosedSprite;
+        _doorBottomRight.sprite = DoorBottomRight ? DoorOpenSprite : DoorClosedSprite;
+        _doorBottomLeft.sprite = DoorBottomLeft ? DoorOpenSprite : DoorClosedSprite;
+        //_doorTopRight.gameObject.SetActive(DoorTopRight);
+        //_doorBottomRight.gameObject.SetActive(DoorBottomRight);
+        //_doorBottomLeft.gameObject.SetActive(DoorBottomLeft);
+    }
+
+    // only used for editor debugging
     void Update()
     {
-        
+        if (Application.isPlaying)
+        {
+            return;
+        }
+        UpdateDoorVisibility();
     }
 }
